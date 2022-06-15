892c9ea96323a8c702c912347992f90c33db9354
KALLEM PAVAN <pawankallem001@gmail.com>
2022-06-15 15:06:09 +0530

    looping with v-for

---
+++ loopint_with_v-for/index.js
@@ -0,0 +1,12 @@
+
+new Vue({
+    el: "#app",
+    data: {
+        arr: [
+            {name: "a", age: 1},
+            {name: "b", age: 2},
+            {name: "c", age: 3},
+            {name: "d", age: 4}
+        ]
+    }
+})
