a05856c6bf2807a5f8755c747935a4fe230d3e06
KALLEM PAVAN <pawankallem001@gmail.com>
2022-06-22 13:12:35 +0530

    restarent app is initiated

---
+++ restro-app/src/components/SignUp.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h1> Sign Up </h1>
+    <form class="signupForm" action="" @submit.prevent="register">
+        <input type="text" placeholder="User Name" v-model="userName" />
+        <input type="text" placeholder="Email" v-model="email" />
+        <input type="text" placeholder="Password" v-model="password" />
+        <input class="submit" type="submit">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+    data() {
+        return {
+            userName: "",
+            email: "",
+            password: "",
+        }
+    },
+    methods: {
+        register: function () {
+            console.log( this.userName,this.email,this.password )
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+.signupForm {
+    display: flex;
+    flex-direction: column;
+    width: 30%;
+    gap: 5px;
+    margin: auto;
+}
+input {
+    height: 40px;
+    text-align: center;
+    border: 1px solid skyblue;
+    border-radius: 10px;
+}
+.submit {
+    background: skyblue;
+    color: #fff;
+    font-size: 120%;
+}
+
+</style>
