cb02a0e6c059ca48a5e0db4b8a992b0b2d7149e0
KALLEM PAVAN <pawankallem001@gmail.com>
2022-06-27 13:33:22 +0530

    added todo working with vuex

--- counter-vuex/src/components/TodoInput.vue
+++ counter-vuex/src/components/TodoInput.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
     <h1> Todo Input </h1>
+    <input type="text" v-model="inputText" />
+    <button @click="addTodo(inputText)"> Add </button>
   </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
 export default {
+    data() {
+        return{
+            inputText: "",
+        }
+    },
+    methods: {
+        ...mapActions(["addTodo"]),
 
+    }
 }
 </script>
 
---
+++ counter-vuex/src/store/index.js
@@ -0,0 +1,36 @@
+import  { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    todos: [
+      {
+        id: 1,
+        title: "todo 1"
+      },
+      {
+        id: 2,
+        title: "todo 2"
+      },
+      {
+        id: 3,
+        title: "todo 3"
+      }
+    ]
+  },
+  getters: {
+    allTodos: (state) => state.todos,
+  },
+  mutations: {
+    add_todo (state, todo) {
+      state.todos.push( todo ),
+      console.log( state.todos )
+    }
+  },
+  actions: {
+    addTodo ({commit}, todo ) {
+      commit( "add_todo", todo)
+    }
+  },
+  modules: {
+  }
+})
